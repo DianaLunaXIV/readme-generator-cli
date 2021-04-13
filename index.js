02a41a558394448bb8a9d97fd841719c276aed7d
Brent B <brentbaughan1@gmail.com>
2021-04-13 14:58:33 -0400

    functional file writing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -14,12 +15,18 @@ const questions = [
     }
 ];
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
-    .then(answers => console.log(answers));
+    .then(answers => writeToFile('test.md', JSON.stringify(answers)));
+    
 }
 
 // Function call to initialize app
