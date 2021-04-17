0c0b6436e57036479e00d10d657fa15f53194451
Brent B <brentbaughan1@gmail.com>
2021-04-17 12:10:40 -0400

    fixed spacing on AGPL license text

--- utils/licenseText.js
+++ utils/licenseText.js
@@ -1,12 +1,13 @@
 const licenseText = {
-        AGPLv3: `GNU AFFERO GENERAL PUBLIC LICENSE
-        Version 3, 19 November 2007
+        AGPLv3: `
+                    GNU AFFERO GENERAL PUBLIC LICENSE
+                    Version 3, 19 November 2007
     
     Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
     Everyone is permitted to copy and distribute verbatim copies
     of this license document, but changing it is not allowed.
     
-            Preamble
+                                Preamble
     
     The GNU Affero General Public License is a free, copyleft license for
     software and other kinds of works, specifically designed to ensure
@@ -57,7 +58,7 @@ const licenseText = {
     The precise terms and conditions for copying, distribution and
     modification follow.
     
-        TERMS AND CONDITIONS
+                            TERMS AND CONDITIONS
     
     0. Definitions.
     
