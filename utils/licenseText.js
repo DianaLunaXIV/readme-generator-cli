dd6c34af6f368fd0a5f55fd9a6ce50d3506603e4
Brent B <brentbaughan1@gmail.com>
2021-04-17 11:59:06 -0400

    tweaked MIT license for better spacing

--- utils/licenseText.js
+++ utils/licenseText.js
@@ -1334,7 +1334,8 @@ const licenseText = {
     the library.  If this is what you want to do, use the GNU Lesser General
     Public License instead of this License.  But first, please read
     <https://www.gnu.org/licenses/why-not-lgpl.html>.`,
-    MIT: `MIT License
+    MIT: `
+    MIT License
   
     Copyright (c) [year] [fullname]
     
